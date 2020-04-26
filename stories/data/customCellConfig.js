08bf9bc0b91a45f4f08791bff27043a4b3027831
Giridharan Rengarajan <18137429+DharanBro@users.noreply.github.com>
2020-04-26 14:54:05 +0530

    added demo stories and storybook deployer (#2)

---
+++ stories/data/customCellConfig.js
@@ -0,0 +1,34 @@
+export const data = {
+    head: ['Name', 'Email', 'Country'],
+    body: [
+        ['Caitlin', 'caitlin@example.com', 'Sweden'],
+        ['Gale', 'gale@example.com', 'Norway'],
+        [{
+            text: "Giridharan",
+            background: "#3F51B4",
+            color: "#FFFFFF"
+        }, {
+            text: "giridharan@gmail.com",
+            background: "#FFFFFF",
+            color: "#333333"
+        }, {
+            text: "India",
+            background: "#FFFFFF",
+            color: "#333333"
+        }],
+        ['Roslyn', 'roslyn@example.com', 'Australia'],
+        ['Reyna', 'reyna@example.com', {
+            text: "Ireland",
+            background: "#F24436",
+            color: "#FFFFFF"
+        }],
+        ['Sheree', 'sheree@example.com', 'Germany'],
+        ['Mueller', 'mueller@example.com', 'United States of America'],
+        ['Gray', {
+            text: "gray@example.com",
+            background: "#FFFFFF",
+            color: "#FBC02D"
+        }, 'Sweden'],
+        ['Fitzpatrick', 'fitzpatrick@example.com', 'France'],
+    ],
+};
---
+++ stories/data/simple.js
@@ -0,0 +1,13 @@
+export const data = {
+    head: ['Name', 'Email', 'Country'],
+    body: [
+        ['Caitlin', 'caitlin@example.com', 'Sweden'],
+        ['Gale', 'gale@example.com', 'Norway'],
+        ['Roslyn', 'roslyn@example.com', 'Australia'],
+        ['Reyna', 'reyna@example.com', 'Ireland'],
+        ['Sheree', 'sheree@example.com', 'Germany'],
+        ['Mueller', 'mueller@example.com', 'United States of America'],
+        ['Gray', 'gray@example.com', 'Sweden'],
+        ['Fitzpatrick', 'fitzpatrick@example.com', 'France'],
+    ],
+};
