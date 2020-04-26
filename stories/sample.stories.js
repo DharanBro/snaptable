d99a849d26735ec87bd1200fcfc59df9f27c9122
dharanbro <18137429+DharanBro@users.noreply.github.com>
2020-04-26 14:52:00 +0530

    added demo stories and storybook deployer

--- stories/sample.stories.js
+++ stories/sample.stories.js
@@ -1,16 +1,46 @@
 import { storiesOf } from "@storybook/html";
+import './style.css'
 import jsPDF from 'jspdf';
 import SnapTable from '../src/index.ts';
-import { data } from './data'
+import { data } from './data/simple'
+import { data as columnSplitData } from './data/columnSplit'
+import { data as customCellConfigData } from './data/customCellConfig'
 
-storiesOf("Sample", module).add("Sample 1", () => {
+function generatePDF(data) {
     const root = document.createElement('div');
     root.setAttribute("class", "root");
+    root.style.width = '100%';
+    root.style.height = '100%';
+
+    const obj = document.createElement('object');
+    obj.setAttribute('width', '100%');
+    obj.setAttribute('height', '100%');
+
     var doc = new jsPDF({ unit: "px" });
     const snapTable = new SnapTable(doc);
     snapTable.writeTable(data);
     var outputString = doc.output('datauristring');
-    var embed = "<embed width='100%' height='100%' src='" + outputString + "'/>"
-    document.write(embed);
+
+    const embed = document.createElement('embed');
+    embed.setAttribute('width', '100%');
+    embed.setAttribute('height', '100%');
+    embed.setAttribute('src', outputString);
+
+    obj.appendChild(embed);
+
+    root.appendChild(obj);
     return root;
+}
+
+
+storiesOf("Demo").add("Simple", () => {
+    return generatePDF(data);
+});
+
+storiesOf("Demo").add("Column Split", () => {
+    return generatePDF(columnSplitData);
+});
+
+storiesOf("Demo").add("Custom Cell", () => {
+    return generatePDF(customCellConfigData);
 });
