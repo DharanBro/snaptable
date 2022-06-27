bf945a6741a92716e91c987546a4614327100153
Giridharan Rengarajan <18137429+DharanBro@users.noreply.github.com>
2022-06-27 19:10:04 +0530

    chore(eslint): add interface-name-prefix override

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typ,escript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/interface-name-prefix": ["off"]
 
     },
     parserOptions: {
